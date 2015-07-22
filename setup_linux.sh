fd82a395b5837932f0529d761876dffb97352f6a
Watanabe Keiichi <nabeon+github@gmail.com>
2015-07-22 14:56:50 +0900

    refactor

--- setup_linux.sh
+++ setup_linux.sh
@@ -2,7 +2,6 @@ echo "install dotfiles..."
 
 # defines
 DOT_FILES="${HOME}/dotfiles"
-SRC_DIR="${DOT_FILES}/src"
 DEPS_DIR="${DOT_FILES}/deps"
 
 echo "[1/5] rm .vim* .zsh* files"
@@ -12,31 +11,31 @@ do
   rm -rf $f
 done
 
-echo "[2/5] symlink: vim...(${SRC_DIR}/vim/*)"
+echo "[2/5] symlink: vim...(${DOT_FILES}/vim/*)"
 echo "ln: vimrc"
-ln -s ${SRC_DIR}/vim/vimrc ${HOME}/.vimrc
+ln -s ${DOT_FILES}/vim/vimrc ${HOME}/.vimrc
 
 echo "ln: gvimrc"
-ln -s ${SRC_DIR}/vim/gvimrc ${HOME}/.gvimrc
+ln -s ${DOT_FILES}/vim/gvimrc ${HOME}/.gvimrc
 
-# for f in "${SRC_DIR}"/vim/*
+# for f in "${DOT_FILES}"/vim/*
 # do
 #   echo "ln: `basename $f`"
 #   ln -s $f ${HOME}/.`basename $f`
 # done
 
-echo "[3/5] symlink: zsh...(${SRC_DIR}/zsh/*)"
+echo "[3/5] symlink: zsh...(${DOT_FILES}/zsh/*)"
 echo "ln: zshrc"
-ln -s ${SRC_DIR}/zsh/zshrc ${HOME}/.zshrc
+ln -s ${DOT_FILES}/zsh/zshrc ${HOME}/.zshrc
 
-# for f in "${SRC_DIR}"/zsh/*
+# for f in "${DOT_FILES}"/zsh/*
 # do
 #   echo "ln: `basename $f`"
 #   ln -s ${f} ${HOME}/.`basename $f`
 # done
 
 echo "[4/5] symlink: git submodules...(${DEPS_DIR}/*)"
-for f in "${DEPS_DIR}"/zprezto "${DEPS_DIR}"/neobundle.vim
+for f in "${DEPS_DIR}"/neobundle.vim
 do
   echo "ln: `basename $f`"
   ln -s "${DEPS_DIR}/`basename $f`" ${HOME}/.`basename $f`
@@ -51,16 +50,16 @@ fi
 mv -f ${HOME}/.neobundle.vim ${HOME}/.vim/bundle/
 mv -f ${HOME}/.vim/bundle/.neobundle.vim ${HOME}/.vim/bundle/neobundle.vim
 
-echo "[5/5] symlink: prezto...(${SRC_DIR}/prezto/*)"
-for f in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/*
-do
-  case "$f" in
-    *\.md) ;;
-	*rc) ;;
-    *) 
-	echo "ln: `basename $f`"
-  	ln -s "$f" ${HOME}/.`basename $f` ;;
-  esac
-done
+# echo "[5/5] symlink: prezto...(${DOT_FILES}/prezto/*)"
+# for f in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/*
+# do
+#   case "$f" in
+#     *\.md) ;;
+# 	*rc) ;;
+#     *) 
+# 	echo "ln: `basename $f`"
+#   	ln -s "$f" ${HOME}/.`basename $f` ;;
+#   esac
+# done
 
 echo "completed."
